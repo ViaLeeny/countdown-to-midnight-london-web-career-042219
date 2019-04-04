aa33cf38d2df979a882c6271b732f00b59450e96
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-04 03:24:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,19 @@
+def countdown (int)
+  int = 10 
+  while int >= 0
+  puts "#{int} SECOND(S)!"
+  int -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+
 def countdown_with_sleep (int)
   int = 10 
   while int >= 0
   puts "#{int} SECOND(S)!"
   int -= 1
   sleep(2)
-end
+  end
 return "HAPPY NEW YEAR!"
 end
-
